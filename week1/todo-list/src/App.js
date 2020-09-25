cf24818089b1644e91c7063dce02cd21e602604e
LJohnson-Bryan <logan.johnson@bryanuniversity.edu>
2020-09-25 12:48:38 -0400

    Todo-List Part 1

---
+++ week1/todo-list/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Do the laundry</p>
+      <input type="checkbox" />
+      <p>Wash the dishes</p>
+      <input type="checkbox" />
+      <p>Finish the website</p>
+      <input type="checkbox" />
+      <p>Eat breakfast</p>
+    </div>
+  )
+}
+
+export default App;
